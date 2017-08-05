98870159e0862c442eaae3fd5fb0155d55aa851d
Beaglefoot <ch-e-st@yandex.ru>
2017-08-05 18:12:45 +0300

    chore: Add tmux pane with test results

--- start_tmux_session.sh
+++ start_tmux_session.sh
@@ -1,6 +1,6 @@
 SESSION_NAME="dev"
 WINDOW_NAME="roguelike"
-WORKING_DIR="~/fcc-roguelike"
+WORKING_DIR="$PWD"
 
 tmux new-session -d -s $SESSION_NAME
 tmux rename-window $WINDOW_NAME
@@ -8,5 +8,8 @@ tmux send-keys "cd $WORKING_DIR; webpack-dev-server" C-m
 
 tmux split-window -v
 tmux send-keys "cd $WORKING_DIR; explorer ." C-m
+
+tmux split-window -h -t {top}
+tmux send-keys "cd $WORKING_DIR; ./run_test_watch.sh" C-m
 
 tmux attach -t $SESSION_NAME
