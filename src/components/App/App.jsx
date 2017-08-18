09f9db3f5f90554491de993c2e2b4341fcaa91d6
Beaglefoot <ch-e-st@yandex.ru>
2017-08-18 17:34:41 +0300

    feat: XP Bar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,18 +4,29 @@ import { connect } from 'react-redux';
 import Grid from '../Grid/Grid';
 import Bar from '../Bar/Bar';
 
-import { app, health } from './App.scss';
+import { app, health, experience, bars } from './App.scss';
 
-const App = ({ hp, maxHP }) => (
+const App = ({ hp, maxHP, xp, xpRange }) => (
   <div className={app}>
-    { hp && <Bar className={health} value={hp} max={maxHP} /> }
+    <div className={bars}>
+      { typeof hp !== 'undefined' && <Bar className={health} value={hp} max={maxHP} /> }
+      {
+        typeof xp !== 'undefined' &&
+          <Bar className={experience} value={xp} max={xpRange.last()} min={xpRange.first()} />
+      }
+    </div>
     <Grid />
   </div>
 );
 
 const mapStateToProps = state => {
   const player = state.get('player');
-  if (player) return { hp: player.get('hp'), maxHP: player.get('maxHP') };
+  if (player) return {
+    hp: player.get('hp'),
+    maxHP: player.get('maxHP'),
+    xp: player.get('xp'),
+    xpRange: player.get('xpRange')
+  };
   return {};
 };
 
