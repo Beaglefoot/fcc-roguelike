1924f1500e92d0267c826969b7cdf422eb65fb58
Beaglefoot <ch-e-st@yandex.ru>
2017-08-24 18:15:13 +0300

    feat: Level Up animation

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -3,17 +3,34 @@ import { connect } from 'react-redux';
 
 import { levelUp } from '../../actions';
 
+import { flyingText } from './Player.scss';
+
 class Player extends React.PureComponent {
+  animateLevelUp() {
+    const p = document.getElementById('player-symbol').getBoundingClientRect();
+    const levelUpElement = document.createElement('div');
+
+    levelUpElement.appendChild(document.createTextNode('Level Up'));
+    levelUpElement.classList.add(flyingText);
+    levelUpElement.style.left = `${p.left}px`;
+    levelUpElement.style.top = `${p.top}px`;
+
+    document.body.appendChild(levelUpElement);
+
+    setTimeout(() => document.body.removeChild(levelUpElement), 5000);
+  }
+
   componentWillReceiveProps({ player, levelUp }) {
     const xp = player.get('xp');
     const xpCeil = player.get('xpRange').last();
-    if (xp >= xpCeil) levelUp();
+    if (xp >= xpCeil) {
+      levelUp();
+      this.animateLevelUp();
+    }
   }
 
   render() {
-    return (
-      <div>@</div>
-    );
+    return <div id="player-symbol">@</div>;
   }
 }
 
